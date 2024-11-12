a2b3011c4e4dc5266528842e2d1bdd2b15620400
RobertChienShiba <robertmadhead0919@gmail.com>
2024-11-13 02:16:30 +0800

    fixed gitlab pipeline error

--- worker.sh
+++ worker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# 啟動 Dramatiq 的背景任務並將輸出寫入 worker.log
-dramatiq src.bg_task > worker.log 2>&1 
-echo "Dramatiq worker started and logging to worker.log"
+# 啟動 Dramatiq 的任務
+echo "Dramatiq worker started"
+dramatiq src.bg_task
