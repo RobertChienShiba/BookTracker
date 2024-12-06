af6d06e08447ad71ce6f653d1346e1577ec0fb02
RobertChienShiba <robertmadhead0919@gmail.com>
2024-12-06 23:35:56 +0800

    fix env var typo

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-if sqlite3 ./bookly.db "SELECT 1" && curl -f "http://localhost:8080/api/$VERSION/docs"; then
+VERSION=$(grep -oP '(?<=version = ")[^"]+' src/__init__.py)
+
+apt-get update && apt-get install -y curl && rm -rf /var/lib/apt/lists/*
+
+if sqlite3 ./bookly.db "SELECT 1" && curl -f "http://localhost:8000/api/$VERSION/docs"; then
   echo "Healthcheck complete successfully"
 else
   exit 1
 fi
+
+unset VERSION
